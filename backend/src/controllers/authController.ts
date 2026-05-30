209d69b917694a37dbe0f87ed83a0e944cc3eb86
Rakshika Sharma <sharmarakshika292@gmail.com>
2026-05-30 18:51:40 +0530

    feat: implement JWT login authentication

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 
 import prisma from "../lib/prisma";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (
   req: Request,
@@ -38,6 +39,64 @@ export const registerUser = async (
 
     res.status(201).json({
       message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+export const loginUser = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // Compare password
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
       user,
     });
   } catch (error) {
